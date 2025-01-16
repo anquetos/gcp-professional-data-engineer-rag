8de8c8983785bec116f6470f930a88e0ff8c0de7
anquetos <t.anquetil@proton.me>
2025-01-16 15:12:31 +0100

    timing_function now use logging to display message.

--- local_rag/timing_functions.py
+++ local_rag/timing_functions.py
@@ -1,7 +1,14 @@
-""" This module contains a decorator that can be used to time the execution of a function. """
+"""This module contains a decorator that can be used to time the execution of a function."""
 
+import logging
 import time
 from functools import wraps
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 
 def timing(func):
     """
@@ -19,7 +26,8 @@ def timing(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        duration = (end_time - start_time)
-        print(f"INFO : '{func.__name__}' function executed in {duration:.5f} seconds.\n\n")
+        duration = end_time - start_time
+        logger.info(f"'{func.__name__}' function executed in {duration:.5f} seconds.")
         return result
+
     return wrapper
