3336d34dcc8ee8224792981791028fedb60748ab
anquetos <t.anquetil@proton.me>
2025-01-16 17:44:34 +0100

    Updated pipeline to take in account pages text exportation.

--- local_rag/preprocessing_pipeline.py
+++ local_rag/preprocessing_pipeline.py
@@ -5,6 +5,7 @@ from pdf_extractor import PDFExtractor
 from text_embedder import TextEmbedder
 
 pdf_filepath = Path(__file__).resolve().parent.parent / "pdf/source.pdf"
+pages_text_filepath = Path(__file__).resolve().parent.parent / "datasets/pages-text.json"
 embeddings_filepath = Path(__file__).resolve().parent.parent / "datasets/embeddings.json"
 
 fontnames = [
@@ -17,11 +18,12 @@ fontnames = [
 
 extractor = PDFExtractor(pdf_filepath)
 extractor.reorder_pages(page_offset=-40)
-extractor.filter_pages_range(2, 3)
-pages_and_text = extractor.extract_pages_text_by_font(fontnames)
+extractor.filter_pages_range(1, 305)
+extractor.extract_pages_text_by_font(fontnames)
+extractor.export_pages_text_to_json(pages_text_filepath)
 extractor.unload_file()
 
-embedder = TextEmbedder(pages_and_text)
-embedder.chunk_text()
-embedder.calculate_embeddings()
-embedder.export_embeddings_to_json(embeddings_filepath)
+# embedder = TextEmbedder(pages_and_text)
+# embedder.chunk_text()
+# embedder.calculate_embeddings()
+# embedder.export_embeddings_to_json(embeddings_filepath)
